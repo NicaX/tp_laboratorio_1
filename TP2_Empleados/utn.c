d1277e4dd1a802e1991ccb08b2b071d57e88b095
LMartinez <40763533+NicaX@users.noreply.github.com>
2018-10-04 04:45:28 +0200

    Add files via upload

---
+++ TP2_Empleados/utn.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+int getInt(char* mensaje)
+{
+    int auxiliar;
+    printf("%s",mensaje);
+    scanf("%d",&auxiliar);
+    return auxiliar;
+}
+
+int esNumericoFlotante(char str[])
+{
+    int i=0;
+    int cantidadPuntos=0;
+    while(str[i] != '\0')
+    {
+        if (str[i] == '.' && cantidadPuntos == 0)
+        {
+            cantidadPuntos++;
+            i++;
+            continue;
+
+        }
+        if(str[i] < '0' || str[i] > '9')
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+int esNumerico(char str[])
+{
+    int i=0;
+    while(str[i] != '\0')
+    {
+        if(str[i] < '0' || str[i] > '9')
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+
+int getStringNumeros(char mensaje[],char input[])
+{
+    char aux[256];
+    getString(mensaje,aux);
+    if(esNumerico(aux))
+    {
+        strcpy(input,aux);
+        return 1;
+    }
+    return 0;
+}
+
+int getStringNumerosFlotantes(char mensaje[],char input[])
+{
+    char aux[256];
+    getString(mensaje,aux);
+    if(esNumericoFlotante(aux))
+    {
+        strcpy(input,aux);
+        return 1;
+    }
+    return 0;
+}
+
+
+int getValidInt(char requestMessage[],char errorMessage[], int lowLimit, int hiLimit)
+{
+    char auxStr[256];
+    int auxInt;
+    while(1)
+    {
+        if (!getStringNumeros(requestMessage,auxStr))
+        {
+            printf ("%s\n",errorMessage);
+            continue;
+        }
+        auxInt = atoi(auxStr);
+        if(auxInt < lowLimit || auxInt > hiLimit)
+        {
+            printf ("El numero del debe ser mayor a %d y menor a %d\n",lowLimit,hiLimit);
+            continue;
+        }
+        return auxInt;
+    }
+}
+
+
+void cleanStdin(void)
+{
+    int c;
+    do
+    {
+        c = getchar();
+    }
+    while (c != '\n' && c != EOF);
+}
+
+int esSoloLetras(char str[])
+{
+    int i=0;
+    while(str[i] != '\0')
+    {
+        if((str[i] != ' ') && (str[i] < 'a' || str[i] > 'z') && (str[i] < 'A' || str[i] > 'Z'))
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+
+void getString(char mensaje[],char input[])
+{
+    printf("%s",mensaje);
+    scanf ("%s", input);
+}
+
+int getStringLetras(char mensaje[],char input[])
+{
+    char aux[256];
+    getString(mensaje,aux);
+    if(esSoloLetras(aux))
+    {
+        strcpy(input,aux);
+        return 1;
+    }
+    return 0;
+}
+void getValidString(char requestMessage[],char errorMessage[], char input[])
+{
+
+    while(1)
+    {
+        if (!getStringLetras(requestMessage,input))
+        {
+            printf ("%s\n",errorMessage);
+            continue;
+        }
+        cleanStdin();
+        break;
+    }
+
+}
+
+
+int getValidFloat(char requestMessage[],char errorMessage[], int lowLimit, int hiLimit)
+{
+    char auxStr[256];
+    float auxFloat;
+    while(1)
+    {
+        if (!getStringNumerosFlotantes(requestMessage, auxStr))
+        {
+            printf ("%s\n",errorMessage);
+            continue;
+        }
+        auxFloat = atof(auxStr);
+        if(auxFloat < lowLimit || auxFloat > hiLimit)
+        {
+            printf ("El numero del debe ser mayor a %d y menor a %d\n",lowLimit,hiLimit);
+            continue;
+        }
+        return auxFloat;
+    }
+}
